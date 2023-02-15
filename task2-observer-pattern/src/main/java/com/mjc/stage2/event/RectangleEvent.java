dd18f268d398dff89e188ffae002632d7d9399b8
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-02-15 16:42:07 +0500

    Implemented Observer design pattern

--- task2-observer-pattern/src/main/java/com/mjc/stage2/event/RectangleEvent.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/event/RectangleEvent.java
@@ -11,6 +11,7 @@ public class RectangleEvent extends EventObject {
         super(source);
     }
 
+
     @Override
     public Rectangle getSource() {
         return (Rectangle) super.getSource();
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,25 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observable;
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+
+public class RectangleObserver implements Observer {
     // Write your code here!
+    @Override
+    public void handleEvent(RectangleEvent event){
+        Rectangle rectangle = event.getSource();
+        int id = rectangle.getId();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+
+        RectangleWarehouse warehouse = RectangleWarehouse.getInstance();
+        warehouse.put(id, new RectangleValues(sideA, sideB));
+    }
+
+
 }
